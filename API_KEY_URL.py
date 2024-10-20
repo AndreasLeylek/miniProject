83b9c4c7bee5a3718b712df57fb669746369e944
AndreasLeylek <andreas-leylek@live.at>
2024-10-20 21:51:02 +0200

    Entferne API_KEY_URL, License und Readme aus der Versionskontrolle

--- API_KEY_URL.py
+++
@@ -1,5 +0,0 @@
-BEARER_TOKEN = (
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWZjZGIxYTM5YTY3OWIzNzA2MTkyMTNjYTg1ODJlZSIsIm5iZiI6MTcyODk4NDI5OC45Mz'
-    'c3NSwic3ViIjoiNjcwZTI0MDI5ZjM1MzFlNmIyNmM1N2UyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.hbM0bT0Gfl'
-    'kitPj0i-KAKgmw_Bo0Jo14ngauBI4V790')
-BASE_URL = 'https://api.themoviedb.org/3'
